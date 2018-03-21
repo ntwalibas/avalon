9d5aa918fef4b9136d527a5e78743ccbf9c258ce
ntwalibas <ntwali.bashige@gmail.com>
2018-03-22 01:55:27 +0530

    Addition: A preliminary function body checker.

---
+++ src/checker/stage_one/decl/function/body_checker.cpp
@@ -0,0 +1,34 @@
+#include <memory>
+#include <vector>
+#include <string>
+
+#include "checker/stage_one/decl/function/body_checker.hpp"
+#include "checker/exceptions/invalid_function.hpp"
+#include "program/ast/stmt/block_stmt.hpp"
+#include "program/ast/decl/function.hpp"
+#include "program/ast/decl/decl.hpp"
+#include "program/ast/program.hpp"
+
+
+namespace avalon {
+    /**
+     * check_body
+     * this function ensures that the function's body is valid
+     */
+    void body_checker::check_body(std::shared_ptr<function>& function_decl, program& prog, const std::string& ns_name) {
+        block_stmt& body = function_decl -> get_body();
+        std::vector<std::shared_ptr<decl> >& declarations = body.get_declarations();
+
+        // make sure we only have variable and statement declarations in the body
+        for(auto& declaration : declarations) {
+            if(declaration -> is_variable() == false and declaration -> is_statement() == false)
+                throw invalid_function(function_decl -> get_token(), "A function can only contain variable declarations and statement declarations.");
+        }
+
+        // make sure the function's body is valid
+
+
+        // make sure each return statement returns an object whose type instance is the same as the function's return type instance
+        
+    }
+}
---
+++ src/checker/stage_one/decl/function/body_checker.hpp
@@ -0,0 +1,28 @@
+#ifndef AVALON_CHECKER_DECL_FUNCTION_BODY_CHECKER_HPP_
+#define AVALON_CHECKER_DECL_FUNCTION_BODY_CHECKER_HPP_
+
+#include <memory>
+#include <string>
+
+#include "program/ast/decl/function.hpp"
+#include "program/ast/program.hpp"
+
+
+namespace avalon {
+    class body_checker {
+    public:
+        /**
+         * check_body
+         * this function ensures that the function's body is valid
+         */
+        static void check_body(std::shared_ptr<function>& function_decl, program& prog, const std::string& ns_name);
+
+    private:
+        /**
+         * we don't allow instatiation of this class
+         */
+        body_checker();
+    };
+}
+
+#endif

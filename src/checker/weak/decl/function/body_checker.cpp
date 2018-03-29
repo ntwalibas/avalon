4d627298f70e090f7d8a06f4f9b52917809c4158
ntwalibas <ntwali.bashige@gmail.com>
2018-03-30 03:32:39 +0530

    Refactoring: We link the function body checker to the block checker and let it handle all requirements of a function's body.

--- src/checker/weak/decl/function/body_checker.cpp
+++ src/checker/weak/decl/function/body_checker.cpp
@@ -1,13 +1,18 @@
 #include <memory>
-#include <vector>
 #include <string>
 
 #include "checker/weak/decl/function/body_checker.hpp"
-#include "checker/exceptions/invalid_function.hpp"
+#include "checker/weak/stmt/block_checker.hpp"
 #include "program/ast/stmt/block_stmt.hpp"
 #include "program/ast/decl/function.hpp"
-#include "program/ast/decl/decl.hpp"
+#include "program/symtable/scope.hpp"
 #include "program/ast/program.hpp"
+
+/* Exceptions */
+#include "program/symtable/exceptions/symbol_already_declared.hpp"
+#include "checker/exceptions/invalid_expression.hpp"
+#include "checker/exceptions/invalid_variable.hpp"
+#include "checker/exceptions/invalid_block.hpp"
 
 
 namespace avalon {
@@ -17,18 +22,26 @@ namespace avalon {
      */
     void body_checker::check_body(std::shared_ptr<function>& function_decl, program& prog, const std::string& ns_name) {
         block_stmt& body = function_decl -> get_body();
-        std::vector<std::shared_ptr<decl> >& declarations = body.get_declarations();
-
-        // make sure we only have variable and statement declarations in the body
-        for(auto& declaration : declarations) {
-            if(declaration -> is_variable() == false and declaration -> is_statement() == false)
-                throw invalid_function(function_decl -> get_token(), "A function can only contain variable declarations and statement declarations.");
-        }
-
-        // make sure the function's body is valid
+        std::shared_ptr<scope>& l_scope = prog.get_scope();
+        type_instance& ret_instance = function_decl -> get_return_type_instance();
 
 
-        // make sure each return statement returns an object whose type instance is the same as the function's return type instance
-        
+        // checking the function is the same as checking the block statement that make the body
+        block_checker checker(ret_instance);
+        try {
+            checker.check(body, l_scope, ns_name);
+        } catch(symbol_already_declared err) {
+            // if variable is declared twice inside the block (or sub-blocks)
+            throw err;
+        } catch(invalid_variable err) {
+            // if a variable declaration fails checking inside the block
+            throw err;
+        } catch(invalid_block err) {
+            // if the block contains anything other that variable and statement declarations
+            throw err;
+        } catch(invalid_expression err) {
+            // if any expression sucks
+            throw err;
+        }        
     }
 }

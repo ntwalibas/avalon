c8cf11eb2ab809256f69686e7bed8f3c835967e3
ntwalibas <ntwali.bashige@gmail.com>
2018-03-29 06:40:48 +0530

    Bug fix: We make sure that all type instances a function depends on (parameters type instances and return type instance) are public if the function is public as well.

--- src/checker/weak/decl/function/header_checker.cpp
+++ src/checker/weak/decl/function/header_checker.cpp
@@ -3,6 +3,7 @@
 #include <string>
 
 #include "checker/weak/decl/function/header_checker.hpp"
+#include "checker/exceptions/invalid_function.hpp"
 #include "checker/weak/decl/type/type_checker.hpp"
 #include "checker/exceptions/invalid_type.hpp"
 #include "program/ast/decl/function.hpp"
@@ -24,9 +25,18 @@ namespace avalon {
 
         // we make sure all the function's parameters type instances are valid
         for(auto& param : params) {
-            type_instance param_type_instance = param.second.get_type_instance();
+            type_instance param_type_instance = param.second.get_type_instance();            
             try {
-                type_instance_checker::complex_check(param_type_instance, l_scope, ns_name, constraints);
+                std::shared_ptr<type> param_instance_type = nullptr;
+                bool is_parametrized = type_instance_checker::complex_check(param_type_instance, l_scope, ns_name, constraints);
+                if(is_parametrized == false)
+                    param_instance_type = param_type_instance.get_type();
+
+                // we make sure that if the function is public then all the type instances it depends are built by public types
+                if(param_instance_type != nullptr && (param_instance_type -> is_public() == false && function_decl -> is_public() == true)) {
+                    throw invalid_function(function_decl -> get_token(), "This function is public but has parameters that depend on private types. All parameters of a public function must be public as well.");
+                }
+
                 // if the type instance checked out, we replace the one on the parameter with the updated on
                 param.second.set_type_instance(param_type_instance);
             } catch(invalid_type err) {
@@ -37,7 +47,16 @@ namespace avalon {
         // we make sure the function's return type instance is valid
         type_instance return_type_instance = function_decl -> get_return_type_instance();
         try {
-            type_instance_checker::complex_check(return_type_instance, l_scope, ns_name, constraints);
+            std::shared_ptr<type> return_instance_type = nullptr;
+            bool is_parametrized = type_instance_checker::complex_check(return_type_instance, l_scope, ns_name, constraints);
+            if(is_parametrized == false)
+                return_instance_type = return_type_instance.get_type();
+
+            // we make sure that if the function is public then the return type instance must be public as well
+            if(return_instance_type != nullptr && (return_instance_type -> is_public() == false && function_decl -> is_public() == true)) {
+                throw invalid_function(function_decl -> get_token(), "This function is public but its return type depends on a private type. The return type must be public as well or the function must be private.");
+            }
+
             // if the type instance checked out, we replace the previous return type instance
             function_decl -> set_return_type_instance(return_type_instance);
         } catch(invalid_type err) {

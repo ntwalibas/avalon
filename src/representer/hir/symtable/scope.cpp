afc7aaac695a68b2063455d44b2e60653ca2a4f2
ntwalibas <ntwali.bashige@gmail.com>
2018-04-23 12:33:46 +0530

    Improvement: If given a subscope, make sure to work all the way up to the topmost parent if what we require is not available in the subscope.

--- src/representer/hir/symtable/scope.cpp
+++ src/representer/hir/symtable/scope.cpp
@@ -51,6 +51,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * add a new type declaration into this scope
      */
     void scope::add_type(const std::string& ns_name, std::shared_ptr<type>& type_decl) {
+        if(m_parent != nullptr)
+            m_parent -> add_type(ns_name, type_decl);
+
         m_dtable.insert_type(ns_name, type_decl);
 
         // we proceed to add constructors to the this scope as well
@@ -70,6 +73,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a type name, find all the types that match the give type irrespective of arity
      */
     std::vector<std::shared_ptr<type> > scope::get_types(const std::string& ns_name, const std::string& type_name) {
+        if(m_parent != nullptr)
+            return m_parent -> get_types(ns_name, type_name);
+
         return m_dtable.get_types(ns_name, type_name);
     }
 
@@ -78,6 +84,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a type instance, we find the type that builds it
      */
     std::shared_ptr<type>& scope::get_type(const std::string& ns_name, const type_instance& type_ins) {
+        if(m_parent != nullptr)
+            return m_parent -> get_type(ns_name, type_ins);
+
         return m_dtable.get_type(ns_name, type_ins);
     }
 
@@ -86,6 +95,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a type name and its arity, return the type that match them
      */
     std::shared_ptr<type>& scope::get_type(const std::string& ns_name, const std::string& type_name, std::size_t arity) {
+        if(m_parent != nullptr)
+            return m_parent -> get_type(ns_name, type_name, arity);
+        
         return m_dtable.get_type(ns_name, type_name, arity);
     }
 
@@ -94,6 +106,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a type declaration, find if an existing type declaration matches it
      */
     bool scope::type_exists(const std::string& ns_name, std::shared_ptr<type>& type_decl) {
+        if(m_parent != nullptr)
+            return m_parent -> type_exists(ns_name, type_decl);
+        
         return m_dtable.type_exists(ns_name, type_decl);
     }
 
@@ -102,6 +117,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a type instance, we find if there is a corresponding type in the symbol table
      */
     bool scope::type_exists(const std::string& ns_name, const type_instance& type_ins) {
+        if(m_parent != nullptr)
+            return m_parent -> type_exists(ns_name, type_ins);
+
         return m_dtable.type_exists(ns_name, type_ins);
     }
 
@@ -110,6 +128,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a type name and its arity, find if an existing type declaration match the same
      */
     bool scope::type_exists(const std::string& ns_name, const std::string& type_name, std::size_t arity) {
+        if(m_parent != nullptr)
+            return m_parent -> type_exists(ns_name, type_name, arity);
+
         return m_dtable.type_exists(ns_name, type_name, arity);
     }
 
@@ -118,6 +139,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a type name and its arity, find if an existing type declaration match the same in all registered namespaces
      */
     bool scope::type_exists(const std::string& type_name, std::size_t arity) {
+        if(m_parent != nullptr)
+            return m_parent -> type_exists(type_name, arity);
+
         return m_dtable.type_exists(type_name, arity);
     }
 
@@ -126,6 +150,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * add a new default constructor into this symbol table
      */
     void scope::add_default_constructor(const std::string& ns_name, default_constructor& def_cons) {
+        if(m_parent != nullptr)
+            return m_parent -> add_default_constructor(ns_name, def_cons);
+
         // make sure there are no namespaces that share the same with this constructor
         if(m_namespaces.count(def_cons.get_name()) > 0)
             throw symbol_can_collide("A constructor cannot have share the same name as a namespace available in this scope.");
@@ -146,6 +173,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * add a new record constructor into this symbol table
      */
     void scope::add_record_constructor(const std::string& ns_name, record_constructor& rec_cons) {
+        if(m_parent != nullptr)
+            return m_parent -> add_record_constructor(ns_name, rec_cons);
+
         // make sure there are no namespaces that share the same with this constructor
         if(m_namespaces.count(rec_cons.get_name()) > 0)
             throw symbol_can_collide("A constructor cannot have share the same name as a namespace available in this scope.");
@@ -166,6 +196,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a constructor name and arity, return the matching default constructor if it exists
      */
     default_constructor& scope::get_default_constructor(const std::string& ns_name, const std::string& cons_name, std::size_t arity) {
+        if(m_parent != nullptr)
+            return m_parent -> get_default_constructor(ns_name, cons_name, arity);
+
         return m_ctable.get_default_constructor(ns_name, cons_name, arity);
     }
 
@@ -174,6 +207,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a constructor name and arity, return the matching record constructor if it exists
      */
     record_constructor& scope::get_record_constructor(const std::string& ns_name, const std::string& cons_name, std::size_t arity) {
+        if(m_parent != nullptr)
+            return m_parent -> get_record_constructor(ns_name, cons_name, arity);
+
         return m_ctable.get_record_constructor(ns_name, cons_name, arity);
     }
 
@@ -182,6 +218,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a type name and its arity, find if an existing default constructor matches the same
      */
     bool scope::default_constructor_exists(const std::string& ns_name, const std::string& cons_name, std::size_t arity) {
+        if(m_parent != nullptr)
+            return m_parent -> default_constructor_exists(ns_name, cons_name, arity);
+
         return m_ctable.default_constructor_exists(ns_name, cons_name, arity);
     }
 
@@ -190,6 +229,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a type name and its arity, find if an existing record constructor matches the same
      */
     bool scope::record_constructor_exists(const std::string& ns_name, const std::string& cons_name, std::size_t arity) {
+        if(m_parent != nullptr)
+            return m_parent -> record_constructor_exists(ns_name, cons_name, arity);
+
         return m_ctable.record_constructor_exists(ns_name, cons_name, arity);
     }
 
@@ -198,6 +240,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a namespace and a function declaration, add the declaration to the scope
      */
     void scope::add_function(const std::string& ns_name, std::shared_ptr<function>& function_decl) {
+        if(m_parent != nullptr)
+            return m_parent -> add_function(ns_name, function_decl);
+
         // make sure that this function shares not the same name with a namespace
         if(m_namespaces.count(function_decl -> get_name()) > 0)
             throw symbol_can_collide("This function has the same name as an existing namespace. This is not allowed.");
@@ -218,6 +263,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a namespace, a function name and its arity, return all functions in that namespace with the same name and arity
      */
     std::vector<std::shared_ptr<function> > scope::get_functions(const std::string& ns_name, const std::string& function_name, size_t arity) {
+        if(m_parent != nullptr)
+            return m_parent -> get_functions(ns_name, function_name, arity);
+
         return m_dtable.get_functions(ns_name, function_name, arity);
     }
 
@@ -226,6 +274,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a namespace and a function declaration, find if that function declaration already exists in that namespace
      */
     bool scope::function_exists(const std::string& ns_name, std::shared_ptr<function>& function_decl) {
+        if(m_parent != nullptr)
+            return m_parent -> function_exists(ns_name, function_decl);
+
         return m_dtable.function_exists(ns_name, function_decl);
     }
 
@@ -234,6 +285,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a namespace, a function name and its arity, find if we have a function with that name and arity in the current namespace
      */
     bool scope::function_exists(const std::string& ns_name, const std::string& function_name, size_t arity) {
+        if(m_parent != nullptr)
+            return m_parent -> function_exists(ns_name, function_name, arity);
+
         return m_dtable.function_exists(ns_name, function_name, arity);
     }
 
@@ -242,6 +296,9 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a namespace and a function name, find if the namespace contains a function with that name
      */
     bool scope::function_exists(const std::string& ns_name, const std::string& function_name) {
+        if(m_parent != nullptr)
+            return m_parent -> function_exists(ns_name, function_name);
+
         return m_dtable.function_exists(ns_name, function_name);
     }
 
@@ -270,15 +327,30 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a namespace and a variable name, return the corresponding variable declaration if it exists in that namespace
      */
     std::shared_ptr<variable>& scope::get_variable(const std::string& ns_name, const std::string& variable_name) {
-        return m_dtable.get_variable(ns_name, variable_name);
+        try {
+            return m_dtable.get_variable(ns_name, variable_name);
+        } catch(symbol_not_found err) {
+            if(m_parent != nullptr)
+                return m_parent -> get_variable(ns_name, variable_name);
+            else
+                throw err;
+        }
     }
 
     /**
      * variable_exists
      * given a namespace and a variable declaration, find if it already exists in this namespace
      */
-    bool scope::variable_exists(const std::string& ns_name, std::shared_ptr<variable>& variable_decl) {
-        return m_dtable.variable_exists(ns_name, variable_decl);
+    bool scope::variable_exists(const std::string& ns_name, std::shared_ptr<variable>& variable_decl) {        
+        bool exists = m_dtable.variable_exists(ns_name, variable_decl);
+        if(exists == false) {
+            if(m_parent != nullptr)
+                return m_parent -> variable_exists(ns_name, variable_decl);
+            else
+                return false;
+        } else {
+            return true;
+        }
     }
 
     /**
@@ -286,7 +358,15 @@ scope::scope() : m_parent(nullptr), m_start_line(0), m_end_line(0) {
      * given a namespace and a variable name, find if there is a variabl declaration for it in this namespace
      */
     bool scope::variable_exists(const std::string& ns_name, const std::string& variable_name) {
-        return m_dtable.variable_exists(ns_name, variable_name);
+        bool exists = m_dtable.variable_exists(ns_name, variable_name);
+        if(exists == false) {
+            if(m_parent != nullptr)
+                return m_parent -> variable_exists(ns_name, variable_name);
+            else
+                return false;
+        } else {
+            return true;
+        }
     }
 
     /**
